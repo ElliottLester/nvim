9c2dfb9b24d9a4c31322b081d28b9b82cedcd19f
Elliott Lester <lester.elliott@gmail.com>
2024-03-12 14:32:48 -0700

    Update plugins
    Add lazygit
    Add neotest
    Add supertab
    Add vim-be-good.lua

---
+++ lua/plugins/neotest.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/fixcursorhold.nvim",
+            "nvim-treesitter/nvim-treesitter",
+            "alfaix/neotest-gtest",
+        },
+    },
+}
