c419c9cef548187ae2b199ff0352d42d03a1571c
Elliott Lester <lester.elliott@gmail.com>
2024-03-20 00:28:10 -0700

    update neotest deps

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -5,6 +5,7 @@ return {
             "nvim-lua/plenary.nvim",
             "antoinemadec/fixcursorhold.nvim",
             "nvim-treesitter/nvim-treesitter",
+            "nvim-neotest/nvim-nio",
             "alfaix/neotest-gtest",
         },
     },
