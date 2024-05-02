7b405c68f4bed58989b58a81d12233dd6a3e1514
Elliott Lester <lester.elliott@gmail.com>
2024-05-02 00:09:56 -0700

    Lazy load Cmake build keymaps

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -1,40 +1,45 @@
 return {
     "Civitasv/cmake-tools.nvim",
-    opts = {
-        cmake_build_directory = "build/${variant:buildType}",
-    },
-    keys = {
-        { "<leader>cb", "+build" },
-        -- operations
-        { "<leader>cbg", "<cmd>CMakeGenerate<cr>", "Configure the project and generate native build system." },
-        { "<leader>cbb", "<cmd>CMakeQuickBuild<cr>", "Quick Build Target." },
-        { "<leader>cbr", "<cmd>CMakeQuickRun<cr>", "Quick Run Target" },
-        { "<leader>cbd", "<cmd>CMakeQuickDebug<cr>", "Quick Debug Target" },
-        { "<leader>cbB", "<cmd>CMakeBuild<cr>", "Use pre-generated build system to actually compile/link targets." },
-        { "<leader>cbR", "<cmd>CMakeRun<cr>", "Run selected launch-able target." },
-        { "<leader>cbD", "<cmd>CMakeDebug<cr>", "Debug selected launch-able target." },
-        { "<leader>cbt", "<cmd>CMakeRunTest<cr>", "Use ctest to run specified test or all of them." },
-        { "<leader>cbc", "<cmd>CMakeClean<cr>", "Cleans all targets, intermediates, and dependencies." },
-        -- configuration
-        { "<leader>cbs", "+settings" },
-        { "<leader>cbsb", "<cmd>CMakeSelectBuildType<cr>", "Select build type" },
-        { "<leader>cbst", "<cmd>CMakeSelectBuildTarget<cr>", "Select target to be built, executable, library." },
-        { "<leader>cbsT", "<cmd>CMakeSelectLaunchTarget<cr>", "Select target to be launched, executable targets." },
-        { "<leader>cbsk", "<cmd>CMakeSelectKit<cr>", "Select kits from 'CMakeKits.json' or 'cmake-kits.json'." },
-        { "<leader>cbsC", "<cmd>CMakeSelectConfigurePreset<cr>", "Select configure preset. 'CMake[User]Presets.json'" },
-        { "<leader>cbsB", "<cmd>CMakeSelectBuildPreset<cr>", "Select build preset. 'CMake[User]Presets.json'" },
-        { "<leader>cbsW", "<cmd>CMakeSelectCwd<cr>", "Opens popup to select a directory with CMakeLists.txt." },
-        { "<leader>cbsO", "<cmd>CMakeSelectBuildDir<cr>", "Opens popup to select a directory for build output." },
-        { "<leader>cbss", "<cmd>CMakeSettings<cr>", "Opens a configuration popup window." },
-        { "<leader>cbsw", "<cmd>CMakeTargetSettings<cr>", "Opens a configuration popup specific to a single target." },
-        -- windows
-        { "<leader>cbw", "+windows" },
-        { "<leader>cbwe", "<cmd>CMakeOpenExecutor<cr>", "Open CMake Executor window." },
-        { "<leader>cbwr", "<cmd>CMakeCloseExecutor<cr>", "Close CMake Executor window." },
-        { "<leader>cbwm", "<cmd>CMakeOpenRunner<cr>", "Open CMake Runner window." },
-        { "<leader>cbwo", "<cmd>CMakeCloseRunner<cr>", "Close CMake Runner window." },
-        { "<leader>cbwi", "<cmd>CMakeInstall<cr>", "Install CMake targets." },
-        { "<leader>cbwz", "<cmd>CMakeStopExecutor<cr>", "Stop CMake Executor process." },
-        { "<leader>cbwf", "<cmd>CMakeShowTargetFiles<cr>", "Show files associated with 'target' Uses telescope." },
-    },
+    lazy = true,
+    config = function()
+        local opts = {
+            cmake_build_directory = "build/${variant:buildType}",
+            cmake_build_options = { "-j 20" },
+        }
+        require("cmake-tools").setup(opts)
+        local wk = require("which-key")
+        wk.register({
+            ["<leader>cb"] = { name = "+cmake" },
+            ["<leader>cbs"] = { name = "+settings" },
+            ["<leader>cbw"] = { name = "+window" },
+            ["<leader>cbg"] = { "<cmd>CMakeGenerate<cr>", "Configure" },
+            ["<leader>cbb"] = { "<cmd>CMakeQuickBuild<cr>", "Quick Build" },
+            ["<leader>cbr"] = { "<cmd>CMakeQuickRun<cr>", "Quick Run" },
+            ["<leader>cbd"] = { "<cmd>CMakeQuickDebug<cr>", "Quick Debug" },
+            ["<leader>cbB"] = { "<cmd>CMakeBuild<cr>", "Build" },
+            ["<leader>cbR"] = { "<cmd>CMakeRun<cr>", "Run" },
+            ["<leader>cbD"] = { "<cmd>CMakeDebug<cr>", "Debug" },
+            ["<leader>cbt"] = { "<cmd>CMakeRunTest<cr>", "Run ctest" },
+            ["<leader>cbc"] = { "<cmd>CMakeClean<cr>", "Clean" },
+            ["<leader>cbx"] = { "<cmd>CMakeStopExecutor<cr>", "Stop" },
+            -- Settings
+            ["<leader>cbsb"] = { "<cmd>CMakeSelectBuildType<cr>", "Select build type" },
+            ["<leader>cbst"] = { "<cmd>CMakeSelectBuildTarget<cr>", "Select target" },
+            ["<leader>cbsT"] = { "<cmd>CMakeSelectLaunchTarget<cr>", "Select launch target" },
+            ["<leader>cbsk"] = { "<cmd>CMakeSelectKit<cr>", "Select kit" },
+            ["<leader>cbsC"] = { "<cmd>CMakeSelectConfigurePreset<cr>", "Select Configure Preset" },
+            ["<leader>cbsB"] = { "<cmd>CMakeSelectBuildPreset<cr>", "Select build preset" },
+            ["<leader>cbsW"] = { "<cmd>CMakeSelectCwd<cr>", "Select a directory, CMakeLists.txt." },
+            ["<leader>cbsO"] = { "<cmd>CMakeSelectBuildDir<cr>", "Select build directory" },
+            ["<leader>cbss"] = { "<cmd>CMakeSettings<cr>", "Settings" },
+            ["<leader>cbsw"] = { "<cmd>CMakeTargetSettings<cr>", "Target settings" },
+            -- Windows
+            ["<leader>cbwe"] = { "<cmd>CMakeOpenExecutor<cr>", "Open CMake Executor window." },
+            ["<leader>cbwr"] = { "<cmd>CMakeCloseExecutor<cr>", "Close CMake Executor window." },
+            ["<leader>cbwm"] = { "<cmd>CMakeOpenRunner<cr>", "Open CMake Runner window." },
+            ["<leader>cbwo"] = { "<cmd>CMakeCloseRunner<cr>", "Close CMake Runner window." },
+            ["<leader>cbwI"] = { "<cmd>CMakeInstall<cr>", "Install CMake targets." },
+            ["<leader>cbwf"] = { "<cmd>CMakeShowTargetFiles<cr>", "show target files in telescope." },
+        })
+    end,
 }
