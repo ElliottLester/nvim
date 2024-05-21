29bcfe39c3b19becb102c50ba8bc2ab0d2b8a6ad
Elliott Lester <lester.elliott@gmail.com>
2024-05-21 12:55:04 -0700

    Make clangd external

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,27 +1,28 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      -- Ensure mason installs the server
-      clangd = {
-        keys = {
-          { "<leader>cK", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            -- Ensure mason installs the server
+            clangd = {
+                mason = false,
+                keys = {
+                    { "<leader>cK", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+                },
+                cmd = {
+                    "clangd",
+                    "--background-index",
+                    "--clang-tidy",
+                    "--header-insertion=iwyu",
+                    "--completion-style=detailed",
+                    "--function-arg-placeholders",
+                    "--fallback-style=Microsoft",
+                },
+                init_options = {
+                    usePlaceholders = true,
+                    completeUnimported = true,
+                    clangdFileStatus = true,
+                },
+            },
         },
-        cmd = {
-          "clangd",
-          "--background-index",
-          "--clang-tidy",
-          "--header-insertion=iwyu",
-          "--completion-style=detailed",
-          "--function-arg-placeholders",
-          "--fallback-style=Microsoft",
-        },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      },
     },
-  },
 }
