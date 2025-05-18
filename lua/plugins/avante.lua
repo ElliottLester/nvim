9704bc2409eaab7339367542e018970360b8f571
Elliott Lester <lester.elliott@gmail.com>
2025-05-17 22:05:38 -0700

    clean up and disable image support

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -10,14 +10,11 @@ return {
             text_generator = {
                 __inherited_from = "openai",
                 endpoint = "https://vm.lester.fi:5000/v1",
-                -- endpoint = "https://api.openai.com/v1",
-                -- model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
-                -- model = "gpt-3.5-turbo",
                 model = "gemma-3-27b-it",
-                timeout = 2000, -- timeout in milliseconds
+                timeout = 5000, -- timeout in milliseconds
+                stream = true,
                 -- temperature = 0, -- adjust if needed
                 -- max_tokens = 4096,
-                -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
                 api_key_name = "",
                 disable_tools = true,
             },
@@ -38,23 +35,23 @@ return {
         "ibhagwan/fzf-lua", -- for file_selector provider fzf
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
         -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-        {
-            -- support for image pasting
-            "HakonHarnes/img-clip.nvim",
-            event = "VeryLazy",
-            opts = {
-                -- recommended settings
-                default = {
-                    embed_image_as_base64 = false,
-                    prompt_for_file_name = false,
-                    drag_and_drop = {
-                        insert_mode = true,
-                    },
-                    -- required for Windows users
-                    use_absolute_path = true,
-                },
-            },
-        },
+        -- {
+        --     -- support for image pasting
+        --     "HakonHarnes/img-clip.nvim",
+        --     event = "VeryLazy",
+        --     opts = {
+        --         -- recommended settings
+        --         default = {
+        --             embed_image_as_base64 = false,
+        --             prompt_for_file_name = false,
+        --             drag_and_drop = {
+        --                 insert_mode = true,
+        --             },
+        --             -- required for Windows users
+        --             use_absolute_path = true,
+        --         },
+        --     },
+        -- },
         {
             -- Make sure to set this up properly if you have lazy=true
             "MeanderingProgrammer/render-markdown.nvim",
