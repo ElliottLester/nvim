9010b8ac2fd29ee06ebc5e29212dd3d86ee8c782
Elliott Lester <lester.elliott@gmail.com>
2026-01-28 09:12:39 -0800

    Add diagnostics toggle

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,30 @@ end)
 vim.keymap.set("n", "<F12>", function()
     require("dap").step_out()
 end)
+
+local function toggle_diag_buf()
+    local bufnr = 0
+    local disabled = vim.diagnostic.is_disabled(bufnr)
+    if disabled then
+        vim.diagnostic.enable(bufnr)
+    else
+        vim.diagnostic.disable(bufnr)
+    end
+end
+
+local function toggle_diag_global()
+    local disabled = vim.diagnostic.is_disabled()
+    if disabled then
+        vim.diagnostic.enable()
+    else
+        vim.diagnostic.disable()
+    end
+end
+
+vim.keymap.set("n", "<leader>xd", toggle_diag_buf, {
+    desc = "Diagnostics: Toggle (Buffer)",
+})
+
+vim.keymap.set("n", "<leader>xD", toggle_diag_global, {
+    desc = "Diagnostics: Toggle (Global)",
+})
