aae20ef3eb7596df4a124fd3ba16792fad2dda79
Elliott <lester.elliott@gmail.com>
2026-02-04 16:45:30 -0800

    Update CodeCompantion

--- lua/plugins/CodeCompanion.lua
+++ lua/plugins/CodeCompanion.lua
@@ -6,10 +6,13 @@ return {
             http = {
                 ["llama.cpp"] = function()
                     return require("codecompanion.adapters").extend("openai_compatible", {
+                        name = "llama_cpp",
+                        formatted_name = "Local llama.cpp",
                         env = {
-                            url = "https://vm.lester.fi:5000", -- replace with your llama.cpp instance
+                            url = "http://llamacpp:5000", -- replace with your llama.cpp instance
                             api_key = "TERM",
                             chat_url = "/v1/chat/completions",
+                            models_endpoint = "/v1/models",
                         },
                         handlers = {
                             parse_message_meta = function(_, data)
@@ -38,4 +41,42 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
     },
+    config = function(_, opts)
+        vim.schedule(function()
+            require("codecompanion").setup(opts)
+            local wk = require("which-key")
+
+            wk.add({
+                {
+                    "<leader>a",
+                    name = "CodeCompanion",
+                    icon = { icon = "🤖", color = "grey" },
+                },
+                {
+                    "<leader>ai",
+                    "<cmd>CodeCompanion<cr>",
+                    desc = "CodeCompanion Inline",
+                    icon = { icon = "", color = "grey" },
+                },
+                {
+                    "<leader>ac",
+                    "<cmd>CodeCompanionChat Toggle<cr>",
+                    desc = "CodeCompanion Chat",
+                    icon = { icon = "💬", color = "grey" },
+                },
+                {
+                    "<leader>aa",
+                    "<cmd>CodeCompanionActions<cr>",
+                    desc = "CodeCompanion Actions",
+                    icon = { icon = "🎬", color = "grey" },
+                },
+                {
+                    "<leader>am",
+                    "<cmd>CodeCompanionCmd<cr>",
+                    desc = "CodeCompanion Cmd",
+                    icon = { icon = ">", color = "grey" },
+                },
+            })
+        end)
+    end,
 }
