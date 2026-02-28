b9b8f73511e85183c63c4d067b762e62bf612094
Elliott Lester <lester.elliott@gmail.com>
2026-02-27 18:02:50 -0800

    Add glsl to conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,22 +4,13 @@ return {
         dependencies = { "clangd_extensions.nvim", "mason.nvim" },
         opts = {
             notify_on_error = true,
-            format_on_save = function(bufnr)
-                -- Disable "format_on_save lsp_fallback" for languages that don't
-                -- have a well standardized coding style. You can add additional
-                -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = { c = true, cpp = true }
-                return {
-                    timeout_ms = 500,
-                    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-                }
-            end,
             formatters_by_ft = {
                 ["c"] = { "clang_format" },
                 ["cpp"] = { "clang_format" },
                 ["objc"] = { "clang_format" },
                 ["objcpp"] = { "clang_format" },
                 ["cuda"] = { "clang_format" },
+                ["glsl"] = { "clang_format" },
                 ["proto"] = { "clang_format" },
                 ["lua"] = { "stylua" },
                 ["rust"] = { "rustfmt" },
