302824eade06ea761fb2947e46e4471d8cd957b6
Elliott <lester.elliott@gmail.com>
2026-04-26 10:36:42 +1000

    Update to test framework to ctest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,25 +1,24 @@
 return {
     { "nvim-neotest/neotest-plenary" },
-    -- { "hfn92/cmake-gtest.nvim", dependencies = { "Civitasv/cmake-tools.nvim" } },
     {
         "nvim-neotest/neotest",
         dependencies = {
-            "nvim-neotest/nvim-nio",
-            "nvim-lua/plenary.nvim",
             "antoinemadec/fixcursorhold.nvim",
-            "nvim-treesitter/nvim-treesitter",
+            "nvim-lua/plenary.nvim",
             "nvim-neotest/nvim-nio",
-            "alfaix/neotest-gtest",
-        },
-        opts = { adapters = { "neotest-python", "neotest-gtest" } },
-        keys = {
+            "nvim-treesitter/nvim-treesitter",
             {
-                "<leader>tc",
-                function()
-                    require("neotest-gtest.executables.ui").configure_executable()
-                end,
-                desc = "configure gtest executable",
+                "orjangj/neotest-ctest",
+                opts = { dap_adapter = "codelldb" },
             },
         },
+        opts = function()
+            return {
+                adapters = {
+                    "neotest-python",
+                    "neotest-ctest",
+                },
+            }
+        end,
     },
 }
