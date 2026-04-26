10505948825e489ac40b750d1f6205cfa3d3dc57
Elliott <lester.elliott@gmail.com>
2026-04-26 10:36:14 +1000

    Fix clipboard

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,21 +1,22 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.g.clipboard = {
-    name = "OSC 52",
-    -- copy = {
-    --     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    --     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    -- },
-    -- copy = {
-    --     ["+"] = { "tmux", "load-buffer", "-" },
-    --     ["*"] = { "tmux", "load-buffer", "-" },
-    -- },
-    -- paste = {
-    --     ["+"] = { "tmux", "save-buffer", "-" },
-    --     ["*"] = { "tmux", "save-buffer", "-" },
-    --     -- paste = {
-    --     --   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    --     --   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-    -- },
-}
+vim.g.clipboard = "osc52"
+-- vim.g.clipboard = {
+--     name = "OSC 52",
+--     -- copy = {
+--     --     ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+--     --     ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+--     -- },
+--     -- copy = {
+--     --     ["+"] = { "tmux", "load-buffer", "-" },
+--     --     ["*"] = { "tmux", "load-buffer", "-" },
+--     -- },
+--     -- paste = {
+--     --     ["+"] = { "tmux", "save-buffer", "-" },
+--     --     ["*"] = { "tmux", "save-buffer", "-" },
+--     --     -- paste = {
+--     --     --   ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+--     --     --   ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+--     -- },
+-- }
